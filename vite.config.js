e18062d2e855fc469da36865a15fa37cb8edd5aa
philip-hru <98641956+philip-hru@users.noreply.github.com>
2026-06-12 12:06:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Freezrik/MyProject-React.git', // НАПРИМЕР: если ссылка freezrik.github.io/diia, то пишите '/diia/'
+  base: '/MyProject-React/', // Вот так правильно! Только название репозитория с косыми чертами
 })
